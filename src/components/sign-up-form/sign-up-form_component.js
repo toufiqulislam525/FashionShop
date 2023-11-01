a26f75c968dd43142e329846f84ee2f70e14b7e1
toufiqulislam525 <toufiqulislam525@gmail.com>
2023-11-01 07:28:10 +0600

    Created stylized form input for signup page

--- src/components/sign-up-form/sign-up-form_component.js
+++ src/components/sign-up-form/sign-up-form_component.js
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import { createAuthUserWithEmailAndPassword, createUserDocumentFromAuth } from "../../utils/firebase/firebase_utils";
+import FormInput from '../form-input/form-input_component';
+
 
 const defaultFormFields = {
     displayName:'',
@@ -62,17 +64,11 @@ const SignUpForm = ()=>{
         <div>
             <h1>Sign up with ur email and password</h1>
             <form onSubmit={handleSubmit}>
-                <label> Display Name </label>
-                <input type="text" required onChange={handleChange} name="displayName" value={displayName} />
-
-                <label> Email</label>
-                <input type="email" required onChange={handleChange} name="email" value={email} />
-
-                <label> Password </label>
-                <input type="password" required onChange={handleChange} name="password" value={password} />
-
-                <label>Confirm Password </label>
-                <input type="password" required onChange={handleChange} name="confirmPassword" value={confirmPassword} />
+                
+                <FormInput label='Name' type="text" required onChange={handleChange} name="displayName" value={displayName} />
+                <FormInput label='Email' type="email" required onChange={handleChange} name="email" value={email} />
+                <FormInput label='Password' type="password" required onChange={handleChange} name="password" value={password} />
+                <FormInput label='Confirm Password' type="password" required onChange={handleChange} name="confirmPassword" value={confirmPassword} />
 
                 <button type='submit'> Sign Up </button>
 
