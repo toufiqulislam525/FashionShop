085eab270ff9a3166827b236d66e54ea5bddf188
toufiqulislam525 <toufiqulislam525@gmail.com>
2023-11-05 07:20:45 +0600

    tweak

---
+++ src/components/product-card/product-card_component.js
@@ -0,0 +1,21 @@
+import './product-card_styles.scss';
+import Button from '../button/button_component';
+
+const ProductCard = ({product})=>{
+    const {name,imageUrl,price} = product;
+    return(
+        <div className='product-card-container'>
+            <img src={imageUrl} alt={`${name}`}/>
+            <div className='footer'>
+                <span className='name'>{name}</span>
+                <span className='price'>{price}$</span>
+
+            </div>
+            <Button >Add to cart</Button>
+        </div>
+
+    );
+
+}
+
+export default ProductCard
