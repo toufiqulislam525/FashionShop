6b7cab8ab3414d3e0d80d4843001a5ce0e83a2d4
toudip525 <toufiqulislam525@gmail.com>
2023-11-06 11:52:43 +0600

    Card Dropdown all functionality

--- src/components/cart-icon/cart-icon_component.js
+++ src/components/cart-icon/cart-icon_component.js
@@ -3,14 +3,15 @@ import {ReactComponent as ShoppingIcon } from '../../assests/shopping-bag.svg';
 import { useContext } from 'react';
 import { CartContext } from '../../contexts/cart_context';
 const CartIcon = () => {
-    const { isCartOpen, setIsCartOpen } = useContext(CartContext);
+    const { isCartOpen, setIsCartOpen, cartCount } = useContext(CartContext);
     const toggleIsCartOpen = () => setIsCartOpen(!isCartOpen);
+    
     
     return(
 
         <div className='cart-icon-container' onClick={toggleIsCartOpen}>
             <ShoppingIcon className='shopping-icon'/>
-            <span className='item-count'>10</span>
+            <span className='item-count'>{cartCount}</span>
 
         </div>
     );
--- src/contexts/cart_context.js
+++ src/contexts/cart_context.js
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
 const addCartItem = (cartItems, productToAdd)=>{
 
@@ -19,13 +19,20 @@ export const CartContext = createContext({
     isCartOpen : false,
     setIsCartOpen : ()=> {},
     cartItems:[],
-    addItemToCart: ()=>{}
+    addItemToCart: ()=>{},
+    cartCount : 0
 });
 
 export const CartProvider = ({children})=>{
 
     const [isCartOpen,setIsCartOpen] = useState(false);
     const [cartItems,setCartItems] = useState([]);
+    const [cartCount, setCartCount] = useState(0);
+    useEffect(()=>{
+        const newCartCount = cartItems.reduce((total, cartItem)=> total + cartItem.quantity, 0);
+        setCartCount(newCartCount);
+    
+    },[cartItems]);
 
     const addItemToCart = (productToAdd) =>{
 
@@ -33,7 +40,7 @@ export const CartProvider = ({children})=>{
     };
 
 
-    const value = {isCartOpen, setIsCartOpen,addItemToCart, cartItems};
+    const value = {isCartOpen, setIsCartOpen,addItemToCart, cartItems,cartCount};
 
     return(
         <CartContext.Provider value = {value}>
