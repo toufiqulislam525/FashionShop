cd029da76ef8911f4a2ac1d4b63b71e7fa01a4cd
toufiqulislam525 <toufiqulislam525@gmail.com>
2023-11-08 21:46:18 +0600

    Categories Route created

---
+++ src/routes/categories-preview/categories-preview_component.js
@@ -0,0 +1,29 @@
+import { Fragment, useContext } from 'react';
+import { CategoriesContext } from '../../contexts/categories_context';
+import CategoryPreview from '../../components/category-preview/category-preview_component';
+import './categories-preview_styles.scss';
+
+
+const CategoriesPreview = ()=>{
+    const { categoriesMap } = useContext(CategoriesContext);
+
+    return(
+        
+        <Fragment>
+            {
+                Object.keys(categoriesMap).map((title) =>{
+                    const products = categoriesMap[title];
+                    return(
+                        <CategoryPreview key={title} title={title} products={products}/>
+                    )
+                    
+                })
+
+            }
+            
+        </Fragment>  
+        
+    );
+}
+
+export default CategoriesPreview;
--- src/routes/shop/shop_component.js
+++ src/routes/shop/shop_component.js
@@ -1,27 +1,20 @@
-import { Fragment, useContext } from 'react';
-import { CategoriesContext } from '../../contexts/categories_context';
-import CategoryPreview from '../../components/category-preview/category-preview_component';
+
+import { Route, Routes } from 'react-router-dom';
+import CategoriesPreview from '../categories-preview/categories-preview_component';
+
 import './shop_styles.scss';
 
 
 const Shop = ()=>{
-    const { categoriesMap } = useContext(CategoriesContext);
+    
 
     return(
         
-        <div className='shop-container'>
-            {
-                Object.keys(categoriesMap).map((title) =>{
-                    const products = categoriesMap[title];
-                    return(
-                        <CategoryPreview key={title} title={title} products={products}/>
-                    )
-                    
-                })
+        <Routes>
+            <Route index element={<CategoriesPreview/>} >
 
-            }
-            
-        </div>  
+            </Route>
+        </Routes>
         
     );
 }
